16d9b43f92207afcb0f93cea1eea320b5ee041d3
MSGanesh23 <2300031569@kluniversity.in>
2025-06-30 16:47:53 +0530

    Commit 6

--- app/page.js
+++ app/page.js
@@ -1,24 +1,23 @@
-// ❌ remove "use client"
-
 import { personalData } from "@/utils/data/personal-data";
+import dynamic from "next/dynamic";
+
 import AboutSection from "./components/homepage/about";
 import Blog from "./components/homepage/blog";
 import ContactSection from "./components/homepage/contact";
-import Education from "./components/homepage/education";
 import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
+// ❗ Dynamically import Education (Lottie uses `document`)
+const Education = dynamic(() => import("./components/homepage/education"), { ssr: false });
+
 async function getData() {
   const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`);
-
   if (!res.ok) throw new Error('Failed to fetch data');
 
   const data = await res.json();
-
   const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
-
   return filtered;
 }
 
